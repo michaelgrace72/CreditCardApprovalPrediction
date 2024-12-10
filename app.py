6f7bc23ac4429875438f636cc829ec01fa3f3169
Azarel Grahandito <skootydack@gmail.com>
2024-12-10 13:21:52 +0700

    fix: removed unused columns

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ model = catboost.CatBoostClassifier()
 if os.path.exists(model_path):
     try:
         model.load_model(model_path)
+        print(model.get_params())
     except catboost.CatBoostError as e:
         print(f"Error loading model: {e}")
 else:
@@ -20,11 +21,13 @@ else:
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
     """
     Endpoint to predict fraud risk.
@@ -34,47 +37,76 @@ def predict():
         # Get JSON data from the request
         input_data = request.json
         if not input_data:
-            return jsonify({'error': 'No input data provided'}), 400
+            return jsonify({"error": "No input data provided"}), 400
 
         # Convert JSON to DataFrame
         df = pd.DataFrame(input_data)
 
         # Validate input structure
         required_columns = [
-            "CODE_GENDER", "FLAG_OWN_CAR", "FLAG_OWN_REALTY", "CNT_CHILDREN",
-            "NAME_EDUCATION_TYPE", "FLAG_WORK_PHONE", "FLAG_PHONE", "FLAG_EMAIL",
-            "CNT_FAM_MEMBERS", "MONTHS_BALANCE", "INCOME", "EMPLOYMENT_YEARS",
-            "EMPLOYMENT_STATUS", "AGE_GROUP", "NAME_INCOME_TYPE_Commercial associate",
-            "NAME_INCOME_TYPE_State servant", "NAME_INCOME_TYPE_Working",
-            "NAME_FAMILY_STATUS_Civil marriage", "NAME_FAMILY_STATUS_Married",
-            "NAME_FAMILY_STATUS_Separated", "NAME_FAMILY_STATUS_Single / not married",
-            "NAME_FAMILY_STATUS_Widow", "NAME_HOUSING_TYPE_Co-op apartment",
-            "NAME_HOUSING_TYPE_House / apartment", "NAME_HOUSING_TYPE_Municipal apartment",
-            "NAME_HOUSING_TYPE_Office apartment", "NAME_HOUSING_TYPE_Rented apartment",
+            "CODE_GENDER",
+            "FLAG_OWN_CAR",
+            "FLAG_OWN_REALTY",
+            "CNT_CHILDREN",
+            "NAME_EDUCATION_TYPE",
+            "FLAG_WORK_PHONE",
+            "FLAG_PHONE",
+            "FLAG_EMAIL",
+            "CNT_FAM_MEMBERS",
+            "INCOME",
+            "EMPLOYMENT_YEARS",
+            "EMPLOYMENT_STATUS",
+            "AGE_GROUP",
+            "NAME_INCOME_TYPE_Commercial associate",
+            "NAME_INCOME_TYPE_State servant",
+            "NAME_INCOME_TYPE_Working",
+            "NAME_FAMILY_STATUS_Civil marriage",
+            "NAME_FAMILY_STATUS_Married",
+            "NAME_FAMILY_STATUS_Separated",
+            "NAME_FAMILY_STATUS_Single / not married",
+            "NAME_FAMILY_STATUS_Widow",
+            "NAME_HOUSING_TYPE_Co-op apartment",
+            "NAME_HOUSING_TYPE_House / apartment",
+            "NAME_HOUSING_TYPE_Municipal apartment",
+            "NAME_HOUSING_TYPE_Office apartment",
+            "NAME_HOUSING_TYPE_Rented apartment",
         ]
 
         # Check if all required columns are present
-        missing_columns = [column for column in required_columns if column not in df.columns]
+        missing_columns = [
+            column for column in required_columns if column not in df.columns
+        ]
         if missing_columns:
-            return jsonify({'error': f'Missing required columns: {", ".join(missing_columns)}'}), 400
+            return jsonify(
+                {"error": f'Missing required columns: {", ".join(missing_columns)}'}
+            ), 400
 
         # Check if the model is trained
         if model.is_fitted():
             # Make predictions
+            print(len(df.columns))
             predictions = model.predict(df)
-            return jsonify({'predictions': predictions.tolist()})
+            print(predictions)
+            return jsonify({"predictions": predictions.tolist()})
         else:
-            return jsonify({'error': 'Model is not trained. Please train the model before making predictions.'}), 400
+            return jsonify(
+                {
+                    "error": "Model is not trained. Please train the model before making predictions."
+                }
+            ), 400
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def health():
     """
     Health check endpoint.
     """
     return jsonify({"status": "ok"}), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
+
